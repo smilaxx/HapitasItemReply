9ae10345daf7ff03a2ce197d4db604933fb1bdf5
smiraxx <s.ky4.asu.0524@gmail.com>
2016-05-20 22:29:46 +0900

    twwt success

--- tweettest.php
+++ tweettest.php
@@ -1,25 +1,21 @@
 <?php
+    require "twitteroauth/autoload.php";
+    use Abraham\TwitterOAuth\TwitterOAuth;
 
 
-    require_once(dirname(__FILE__)."/twitteroauth/twitteroauth/twitteroauth.php");
 
     // developer info
-    $consumerKey       = 'hxlPpJHDZQU28NvsFVu0WMWcf';
-    $consumerSecret    = 'ho1vt4Z9YTY8qUxR7Nt7hCJNQucIkYH03c8NXmwPq0MTpm1fTB';
-    $accessToken       = '3274641626-693pHd2OBx3nitRw4vXJqpfgT4OyAhz0AdAKcLC';
-    $accessTokenSecret = 'DPpTzAK3eiMcfhrGqoQgHQ41NX0B2mHFsGMG1rzZl5hmM’;
+    $consumerKey       = "hxlPpJHDZQU28NvsFVu0WMWcf";
+    $consumerSecret    = "ho1vt4Z9YTY8qUxR7Nt7hCJNQucIkYH03c8NXmwPq0MTpm1fTB";
+    $accessToken       = "3274641626-693pHd2OBx3nitRw4vXJqpfgT4OyAhz0AdAKcLC";
+    $accessTokenSecret = "DPpTzAK3eiMcfhrGqoQgHQ41NX0B2mHFsGMG1rzZl5hmM";
 
     $oAuth = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 
-    // oAuth認証を利用し、twitterに投稿する
-    $TWITTER_STATUS_UPDATE_URL = "http://api.twitter.com/1.1/statuses/update.json";
-    $method  = 'POST';
-    $message = 'テスト';
-
-    $response = $oAuth->post('statuses/update', array('status' => $message));
+    $res = $oAuth->post("statuses/update", array("status" => "はしもと"));
 
     // 結果出力
-    var_dump($response);
+    var_dump($res);
 
 
 
